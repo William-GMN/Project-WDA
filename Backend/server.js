7ea9de8ca52c87bdae93219695700ffbdb4fc66d
Sebastian Svensson <svse22pu@student.ju.se>
2025-02-25 09:55:00 +0100

    Fixed redirect home /seb

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express()
 const PORT = 8080;
+const path = require("path")
 //const cors = require("cors")
 
+// Routing
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/HTML/Home.html"))
+})
+
+// Basic server setup
 app.get("/", (req, res) => {
     res.send("Welcome to the REST API")
 })
@@ -11,4 +18,4 @@ app.listen(PORT, () => {
     console.log(`Server running att http://${PORT}`)
 })
 
-app.use("/", express.static('public'))
+app.use(express.static(path.join(__dirname, "../Frontend")))
